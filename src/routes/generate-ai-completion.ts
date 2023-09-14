cf53163778d0ab4c8de354a7449ca0b6276ec818
Gabriel Vicente <gabriel.vicent@hotmail.com>
2023-09-14 15:42:46 -0300

    added seed file + adjustments

--- src/routes/generate-ai-completion.ts
+++ src/routes/generate-ai-completion.ts
@@ -2,15 +2,16 @@ import { FastifyInstance } from "fastify";
 import { z } from 'zod'
 import { prisma } from "../lib/prisma";
 import { openai } from "../lib/openai";
+import { streamToResponse, OpenAIStream } from 'ai'
 export const generateAICompletionRoute = async (app: FastifyInstance) => {
   app.post("/ai/complete", async (req, reply) => {
     const bodySchema = z.object({
       videoId: z.string(),
-      template: z.string(),
+      prompt: z.string(),
       temperature: z.number().min(0).max(1).default(0.5),
     })
 
-    const { videoId, template, temperature } = bodySchema.parse(req.body)
+    const { videoId, prompt, temperature } = bodySchema.parse(req.body)
 
     const video = await prisma.video.findFirstOrThrow({
       where: {
@@ -24,7 +25,7 @@ export const generateAICompletionRoute = async (app: FastifyInstance) => {
       })
     }
 
-    const promptMessage = template.replace('{transcription}', video.transcription)
+    const promptMessage = prompt.replace('{transcription}', video.transcription)
 
     const response = await openai.chat.completions.create({
       model: 'gpt-3.5-turbo-16k',
@@ -33,8 +34,15 @@ export const generateAICompletionRoute = async (app: FastifyInstance) => {
         role: "user",
         content: promptMessage
       }],
+      stream: true
     })
 
-    return response
+    const stream = OpenAIStream(response)
+    streamToResponse(stream, reply.raw, {
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
+      }
+    })
   })
 }
